dc977834383dc8fbdaa05999c6f32f89b78fa739
trungnx <xuantrung@bee-system.vn>
2025-03-27 10:05:32 +0700

    Fix student report

--- DevEduManager/Screens/frmBaoCaoHocVienTheoThang.cs
+++ DevEduManager/Screens/frmBaoCaoHocVienTheoThang.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
-using BusinessLogic;
 using System.Threading;
-using QuanLyHocVien.Reports;
-using Microsoft.Reporting.WinForms;
 using System.Collections.Generic;
 using System.Data;
 using Enity.Models;
@@ -23,35 +20,35 @@ namespace DevEduManager.Screens
 
         private void btnTaoBaoCao_Click(object sender, EventArgs e)
         {
-            frmReport frm = new frmReport();
+            //frmReport frm = new frmReport();
 
-            List<ReportParameter> _params = new List<ReportParameter>()
-            {
-                new ReportParameter("CenterName", GlobalSettings.CenterName),
-                new ReportParameter("CenterWebsite", GlobalSettings.CenterWebsite),
-                new ReportParameter("Month", dateThang.Value.Month.ToString()),
-                new ReportParameter("Year", dateThang.Value.Year.ToString())
-            };
+            //List<ReportParameter> _params = new List<ReportParameter>()
+            //{
+            //    new ReportParameter("CenterName", GlobalSettings.CenterName),
+            //    new ReportParameter("CenterWebsite", GlobalSettings.CenterWebsite),
+            //    new ReportParameter("Month", dateThang.Value.Month.ToString()),
+            //    new ReportParameter("Year", dateThang.Value.Year.ToString())
+            //};
 
-            frm.ReportViewer.LocalReport.ReportEmbeddedResource = "QuanLyHocVien.Reports.rptBaoCaoHocVienGhiDanhTheoThang.rdlc";
+            //frm.ReportViewer.LocalReport.ReportEmbeddedResource = "QuanLyHocVien.Reports.rptBaoCaoHocVienGhiDanhTheoThang.rdlc";
 
-            dsSource.dtBaoCaoHocVienTheoThangDataTable dt = new dsSource.dtBaoCaoHocVienTheoThangDataTable();
-            var query = PhieuGhiDanh.BaoCaoHocVienGhiDanhTheoThang(dateThang.Value.Month, dateThang.Value.Year);
-            foreach (var i in query)
-            {
-                dt.Rows.Add(i.MaHV, i.TenHV, i.GioiTinhHV, i.NgayGhiDanh, i.TenKH);
-            }
+            //dsSource.dtBaoCaoHocVienTheoThangDataTable dt = new dsSource.dtBaoCaoHocVienTheoThangDataTable();
+            //var query = PhieuGhiDanh.BaoCaoHocVienGhiDanhTheoThang(dateThang.Value.Month, dateThang.Value.Year);
+            //foreach (var i in query)
+            //{
+            //    dt.Rows.Add(i.MaHV, i.TenHV, i.GioiTinhHV, i.NgayGhiDanh, i.TenKH);
+            //}
 
-            frm.ReportViewer.LocalReport.DataSources.Clear();
-            frm.ReportViewer.LocalReport.DataSources.Add(new ReportDataSource("ds", (DataTable)dt));
+            //frm.ReportViewer.LocalReport.DataSources.Clear();
+            //frm.ReportViewer.LocalReport.DataSources.Add(new ReportDataSource("ds", (DataTable)dt));
 
-            frm.ReportViewer.LocalReport.SetParameters(_params);
-            frm.ReportViewer.LocalReport.DisplayName = "Báo cáo học viên ghi danh theo tháng";
-            frm.Text = "Báo cáo học viên ghi danh theo tháng";
+            //frm.ReportViewer.LocalReport.SetParameters(_params);
+            //frm.ReportViewer.LocalReport.DisplayName = "Báo cáo học viên ghi danh theo tháng";
+            //frm.Text = "Báo cáo học viên ghi danh theo tháng";
 
 
 
-            frm.ShowDialog();
+            //frm.ShowDialog();
         }
 
         private void gridBaoCao_RowsRemoved(object sender, DataGridViewRowsRemovedEventArgs e)
@@ -66,20 +63,20 @@ namespace DevEduManager.Screens
 
         private void btnXem_Click(object sender, EventArgs e)
         {
-            Thread th = new Thread(() =>
-            {
-                object dshv = PhieuGhiDanh.BaoCaoHocVienGhiDanhTheoThang(dateThang.Value.Month, dateThang.Value.Year);
+            //Thread th = new Thread(() =>
+            //{
+            //    object dshv = PhieuGhiDanh.BaoCaoHocVienGhiDanhTheoThang(dateThang.Value.Month, dateThang.Value.Year);
 
-                gridBaoCao.Invoke((MethodInvoker)delegate
-                {
-                    gridBaoCao.DataSource = dshv;
-                });
-            });
+            //    gridBaoCao.Invoke((MethodInvoker)delegate
+            //    {
+            //        gridBaoCao.DataSource = dshv;
+            //    });
+            //});
 
-            th.Start();
+            //th.Start();
 
-            if (MessageBox.Show("Bạn có muốn tạo báo cáo?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-                btnTaoBaoCao_Click(sender, e);
+            //if (MessageBox.Show("Bạn có muốn tạo báo cáo?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            //    btnTaoBaoCao_Click(sender, e);
         }
     }
 }
